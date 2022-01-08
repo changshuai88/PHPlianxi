17897e817fe45126289e0bcd9bae2c69f6e4f6cc
2233282817@qq.com <2233282817@qq.com>
2022-01-08 10:36:49 +0800

    拉取composer框架并引入路由组件

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require('vendor/autoload.php');
+
+use NoahBuscher\Macaw\Macaw;
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '69a3b923a0897ca7c34c1d8ebe1753743ca683c6',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '69a3b923a0897ca7c34c1d8ebe1753743ca683c6',
+            'dev_requirement' => false,
+        ),
+        'noahbuscher/macaw' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../noahbuscher/macaw',
+            'aliases' => array(),
+            'reference' => 'ade32370a3d09f1fa498de254411d1b6b7be1877',
+            'dev_requirement' => false,
+        ),
+    ),
+);
