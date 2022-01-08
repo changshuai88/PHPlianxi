af33c72564b5db5e68e6d1cf17651c10254f8f25
2233282817@qq.com <2233282817@qq.com>
2022-01-08 15:06:33 +0800

    同样的代码，为什么第二次会出错呢？

--- app/controllers/BaseControllers.php
+++ app/controllers/BaseControllers.php
@@ -1,1 +1,22 @@
 <?php
+    namespace controllers;
+
+    class BaseControllers{
+        protected function success($url,$mess){
+            echo "<script>";
+            echo "alert('{$mess}');";
+            if(!empty($url)){
+                echo "location.href='{$url}';";
+            }
+            echo "</script>";
+        }
+
+        protected function error($url,$mess){
+            echo "<script>";
+            echo "alert('ERROR:{$mess}');";
+            if(!empty($url)){
+                echo "location.href='{$url}';";
+            }
+            echo "</script>";
+        }
+    }
--- app/controllers/admin/admin.php
+++ app/controllers/admin/admin.php
@@ -1,12 +1,15 @@
 <?php
 namespace admin;
+use controllers\BaseControllers;
 
-class Demo{
+class Demo extends BaseControllers{
     function index(){
         echo "index.......";
+        $this->success('/home',"成功");
     }
 
     function add(){
         echo "add....";
+        $this->error('/home/add',"失败");
     }
 }
