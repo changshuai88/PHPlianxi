ef00fa46c2469ebd8e7264c7b7c2a99542d76887
2233282817@qq.com <2233282817@qq.com>
2022-02-13 19:57:40 +0800

    地基基本打完

--- app/controllers/admin/index.php
+++ app/controllers/admin/index.php
@@ -6,7 +6,6 @@
             
 
             $this->assign('name','cjs');
-            $this->assign('gj','https://img0.baidu.com/it/u=4241179165,3853519413&fm=253&fmt=auto&app=138&f=JPEG?w=741&h=500');
 
             $this->display("index/index");
         }
