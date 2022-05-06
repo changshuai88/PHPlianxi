e348ece59e10d2e1b634b4eecdd057bcecc56fac
2233282817@qq.com <2233282817@qq.com>
2022-05-06 21:24:41 +0800

    产品展示页

--- app/controllers/admin/login.php
+++ app/controllers/admin/login.php
@@ -9,7 +9,7 @@
    
        function index(){
 
-        // $pw = md5(md5(ew_admin));
+        // $pw = md5(md5(ew_shuai888));
         // dd($pw);
         $url = getCurUrl();
 
--- app/controllers/home/product.php
+++ app/controllers/home/product.php
@@ -1,10 +1,15 @@
 <?php
 namespace home;
+use models\basedao;
 
 class Product extends Home{
     function index(){
 
+        $db = new BaseDao();
+        $db->query("set names utf8");
         
+        $goods = $db->select("goods",["id","name","partno","price",'weight','date','bid','mid']);
+
         $this->assign('title','产品中心');
         $this->display("product/product");
 
